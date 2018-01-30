f45889758eb8fda1392543c907d123a9556f5340
HassanRH <hassan---06@hotmail.com>
2018-01-30 14:19:15 +0100

    ...

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        /// <summary>
+        /// Her kan vi se hvor meget mælk der skal være i Flatwhite
+        /// </summary>
+        /// <returns></returns>
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        /// <summary>
+        /// Her kan vi se hvor meget prisen er på flatwhite
+        /// </summary>
+        /// <returns></returns>
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        /// <summary>
+        /// Her kan vi se hvad styren på kaffen er i dette tilflde er den mild.
+        /// </summary>
+        /// <returns></returns>
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
